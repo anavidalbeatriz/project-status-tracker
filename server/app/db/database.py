a90de0bd806911a7f2115c1ae31d37cb73e2b299
Ana Vidal <Ana.Vidal@amway.com>
2026-02-10 12:19:26 -0300

    Fix database for Railway

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -8,11 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
-
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
